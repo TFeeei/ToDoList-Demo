c7a2627e21d9636e9cc9922d0c37889df906c532
fei <tf2023job@gmail.com>
2023-02-15 23:44:56 +0900

    1. basic function

---
+++ vue.js
@@ -0,0 +1,127 @@
+new Vue({
+    el: "#root",
+    data: {
+        toDoList: [],
+
+        totalNum: "",
+        alertShow: false,
+        inputTitle: "",
+        inputContent: "",
+        currentClickIndex: "",
+
+        newTitle: '',
+        newContent: '',
+    },
+
+    mounted() {
+        this.getToDoList();
+    },
+
+    methods: {
+        // データを獲得する
+        getToDoList() {
+            axios.get('./getData.php')
+                .then((res) => {
+                    this.toDoList = res.data;
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        },
+
+        // Todoの新規作成
+        addTodo() {
+            if (this.newTitle && this.newContent) {
+                let addData = {
+                    title: this.newTitle,
+                    content: this.newContent,
+                }
+                axios.post('./insertData.php', addData)
+                    .then(res => {
+                        console.log(res.data);
+                    }).catch(err => {
+                        console.log(err);
+                    }).then(() => {
+                        this.getToDoList()
+                    })
+            } else {
+                alert("タイトルと内容を両方入力してください")
+            }
+        },
+
+        // Todoの編集
+        editTodo(item, index) {
+            this.alertShow = true
+            this.inputTitle = item.title
+            this.inputContent = item.content
+            this.currentClickIndex = parseInt(this.toDoList[index].ID)
+        },
+        // 編集を確認する
+        sureClick() {
+            if (this.inputTitle && this.inputContent) {
+                let updateData = {
+                    title: this.inputTitle,
+                    content: this.inputContent,
+                    id: this.currentClickIndex
+                }
+                axios.post('./updateData.php', updateData)
+                    .then(res => {
+                        console.log(res.data);
+                    }).catch(err => {
+                        console.log(err);
+                    }).then(() => {
+                        this.alertShow = false
+                        this.getToDoList()
+                    })
+            } else {
+                alert("タイトルと内容を両方入力してください")
+            }
+
+        },
+        // 編集をキャンセルする
+        cancelClick() {
+            this.alertShow = false
+        },
+
+        // Todoの削除
+        deleteTodo(item, index) {
+            // 把现在点击的todo项目的id传值
+            this.currentClickIndex = parseInt(this.toDoList[index].ID)
+
+            axios.post('./deleteData.php', {
+                    id: this.currentClickIndex
+                })
+                .then(res => {
+                    console.log(res.data);
+                }).catch(err => {
+                    console.log(err);
+                }).then(() => {
+                    this.getToDoList()
+                })
+        },
+
+    },
+
+    filters: {
+        // 日時データの処理
+        dateFormate(value) {
+            const date = new Date(value);
+            const year = date.getFullYear();
+            const month = date.getMonth() + 1;
+            const day = date.getDate();
+            return year + '年' + month + '月' + day + '日';
+        }
+    }
+
+
+
+
+    // 日時データの処理
+    // this.newCreatedAt = moment(new Date()).format('YYYY-MM-DD hh:mm:ss')
+    // this.newCreatedAt = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    // this.newCreatedAt = new Date().toLocaleString()
+    // this.newCreatedAt = new Date().getTime();
+    // console.log(typeof this.newCreatedAt);
+
+
+})
