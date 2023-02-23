a76a132b5c8015f65b68d09bb223eb54a29ed555
fei <tf2023job@gmail.com>
2023-02-23 21:01:43 +0900

    6

--- DatabaseHandler.php
+++ DatabaseHandler.php
@@ -26,32 +26,32 @@ class DatabaseHandler
             while ($row = $result->fetch_assoc()) {
                 $rows[] = $row;
             }
-            echo json_encode($rows);
+            echo json_encode($rows);  // 取得したデータをJSON形式で出力
         } else {
-            echo "No results";
+            die("No results found.");
         }
     }
 
     function insertData($title, $content)
     {
-        $createdAt = $this->getCurrentTime(); // データを挿入する時間を取得
+        $createdAt = $this->getCurrentTime(); // データの挿入時間を取得
 
         $sql = "INSERT INTO posts VALUES(DEFAULT, ?, ?, ?, ?)";
         $stmt = $this->conn->prepare($sql);
-        $stmt->bind_param("ssss", $title, $content, $createdAt, $createdAt);
+        $stmt->bind_param("ssss", $title, $content, $createdAt, $createdAt); // パラメータをバインド
         $stmt->execute();
-        echo "Data inserted successfully";
+        echo "Data inserted successfully.";
     }
 
     function updateData($todoId, $title, $content)
     {
-        $updatedAt = $this->getCurrentTime(); // データを更新する時間を取得
+        $updatedAt = $this->getCurrentTime(); // データの更新時間を取得
 
         $sql = "UPDATE posts SET title = ?, content = ?, updated_at = ? WHERE ID = ?";
         $stmt = $this->conn->prepare($sql);
         $stmt->bind_param("sssi", $title, $content, $updatedAt, $todoId);
         $stmt->execute();
-        echo "Data updated successfully";
+        echo "Data updated successfully.";
     }
 
     function deleteData($todoId)
@@ -60,7 +60,7 @@ class DatabaseHandler
         $stmt = $this->conn->prepare($sql);
         $stmt->bind_param("i", $todoId);
         $stmt->execute();
-        echo "Data deleted successfully";
+        echo "Data deleted successfully.";
     }
 
     // 現在の時間を取得する
@@ -79,7 +79,7 @@ class DatabaseHandler
 
 
 $db = new DatabaseHandler();
-$data = json_decode(file_get_contents("php://input"), true);
+$data = json_decode(file_get_contents("php://input"), true); // POSTされたJSONデータから配列へ変換
 $action = $_GET['action'] ?? $_POST['action'] ?? null;
 
 switch ($action) {
@@ -97,6 +97,7 @@ switch ($action) {
         break;
 }
 
+// XSS対策
 function filterXSS($data)
 {
     return htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
